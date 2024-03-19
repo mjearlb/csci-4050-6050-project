df5b29083ea9cbaf5475c810548f0cab3e1c02a6
mjearlb <milobauman@gmail.com>
2024-03-19 16:52:45 -0400

    Moved var's to .env

--- server/database.js
+++ server/database.js
@@ -1,14 +1,22 @@
 import mysql from 'mysql2'
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'swe-proj-sql-user',
-    password: '/Liang1234',
-    database: 'csci_swe_project'
+    host: process.env.MYSQL_HOST, // 127.0.0.1;
+    user: process.env.MYSQL_USER, // 'swe-proj-sql-user'
+    password: process.env.MYSQL_PASSWORD, // '/Liang1234'
+    database: process.env.MYSQL_DATABASE // 'csci_swe_project'
 }).promise()
 
-const result = await pool.query("SELECT * FROM notes")
-const rows = result[0]
-// or: const [rows] = await pool.query("SELECT * FROM notes")
-console.log(rows)
+async function getNotes () {
+    const result = await pool.query("SELECT * FROM notes")
+    const rows = result[0]
+    // or: const [rows] = await pool.query("SELECT * FROM notes")
+    return rows
+}
+
+const notes = await getNotes()
+console.log(notes)
 
