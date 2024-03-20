3b556f600f653467dec77ea453bd61e6e684a792
mjearlb <milobauman@gmail.com>
2024-03-20 14:06:27 -0400

    Made connection.js use .env

--- server/connection.js
+++ server/connection.js
@@ -1,10 +1,14 @@
 var mysql = require('mysql');
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+dotenv.config()
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "swe-proj-sql-user",
-  password: "/Liang1234"
-});
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST, // 127.0.0.1;
+    user: process.env.MYSQL_USER, // 'swe-proj-sql-user'
+    password: process.env.MYSQL_PASSWORD, // '/Liang1234'
+    database: process.env.MYSQL_DATABASE // 'csci_swe_project'
+}).promise()
 
 con.connect(function(err) {
   if (err) throw err;
