699f3dcdc23d9d90d11e7c8c2a03aa52aaeb919a
mjearlb <milobauman@gmail.com>
2024-03-20 09:45:08 -0400

    server.js now displays our public directory

--- example/server.js
+++ example/server.js
@@ -2,13 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-}) // allows http get request which will return Hello World
-
-app.get("/goodbye", (req, res) => { // REQuest and RESponse
-    res.send("<h1>Goodbye World!</h1>")
-})
+app.use(express.static("public"))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port} and http://localhost:${port}/goodbye`)
