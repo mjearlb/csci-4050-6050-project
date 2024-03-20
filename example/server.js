8a671ae932bc00502ca4cff223fbabc1d4cbf572
mjearlb <milobauman@gmail.com>
2024-03-20 12:56:30 -0400

    Added Date() to default page

--- example/server.js
+++ example/server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static("public"))
+app.get('/', (req, res) => {
+    res.send(`<h1>${Date()}</h1>`)
+})
+
+app.use(express.static("public")) 
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port} and http://localhost:${port}/goodbye`)
