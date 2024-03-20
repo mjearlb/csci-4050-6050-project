cac6d526b99e8038304ad44b59e1a4b1638c78c3
mjearlb <milobauman@gmail.com>
2024-03-20 13:29:49 -0400

    Added functionality for localhost:8080/notes/[id of note]

--- server/app.js
+++ server/app.js
@@ -4,9 +4,15 @@ import {getNotes, getNote, createNote} from './database.js'
 
 const app = express()
 
-app.get("/notes", (req, res) => {
+app.get("/notes", async (req, res) => {
     const notes = await getNotes()
     res.send(notes)
+})
+
+app.get("/notes/:id", async (req, res) => {
+    const id = req.params.id
+    const note = await getNote(id)
+    res.send(note)
 })
 
 app.use((err, req, res, next) => {
