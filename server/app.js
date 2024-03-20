f6743f6d5a67c41cf5c01fe45276f5349f4995c8
mjearlb <milobauman@gmail.com>
2024-03-20 13:44:27 -0400

    Added post request

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ import {getNotes, getNote, createNote} from './database.js'
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/notes", async (req, res) => {
     const notes = await getNotes()
     res.send(notes)
@@ -14,6 +16,12 @@ app.get("/notes/:id", async (req, res) => {
     const note = await getNote(id)
     res.send(note)
 })
+
+app.post("/notes", async (req, res) => {
+    const {title, content} = req.body
+    const note = await createNote(title, content)
+    res.status(201).send(note)
+}) 
 
 app.use((err, req, res, next) => {
     console.error(err.stack)
