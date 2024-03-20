842320bd820efe86b0020ddfa9e581e1fc436e53
mjearlb <milobauman@gmail.com>
2024-03-19 22:04:52 -0400

    Added functions to database.js

--- server/database.js
+++ server/database.js
@@ -10,13 +10,27 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE // 'csci_swe_project'
 }).promise()
 
-async function getNotes () {
+async function getNotes() {
     const result = await pool.query("SELECT * FROM notes")
     const rows = result[0]
     // or: const [rows] = await pool.query("SELECT * FROM notes")
     return rows
-}
+} // getNotes
+
+async function getNote(id) {
+    const [rows] = await pool.query("SELECT * FROM notes WHERE id = ?", [id]) // don't do ${id} instead of ?
+    return rows
+} // getNote
 
 const notes = await getNotes()
-console.log(notes)
+// console.log(notes)
+
+async function createNote(title, contents) {
+    const [result] = await pool.query("INSERT INTO notes (title, contents) VALUE (?, ?)", [title, contents])
+    const id = result.insertId
+    return getNote(id)
+} // createNote
+
+const result = await createNote('test', 'test')
+console.log(result)
 
