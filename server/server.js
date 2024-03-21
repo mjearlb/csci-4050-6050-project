e848bfd7d1ff3805bf18ef9143d7afc3b34fb4e0
mjearlb <milobauman@gmail.com>
2024-03-21 13:58:37 -0400

    Added /date to example page

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 3000;
 
 const {getUsers, getUser, createUser} = require('./database.js');
 
+app.get('/date', (req, res) => {
+    res.send(`<h1>${Date()}</h1>`)
+})
 
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`)
