9c14c7376820f2a8a0454c1e6c8a723ea4bfdfb1
mjearlb <milobauman@gmail.com>
2024-03-25 22:39:02 -0400

    Updated MySQL database to include new tables attractions, merchandise, and cart.

--- database/proj_database.sql
+++ database/proj_database.sql
@@ -16,6 +16,67 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `attractions`
+--
+
+DROP TABLE IF EXISTS `attractions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `attractions` (
+  `attr_id` int NOT NULL AUTO_INCREMENT,
+  `attr_name` varchar(255) NOT NULL,
+  `date_installed` date NOT NULL,
+  `date_inspected` date NOT NULL,
+  `description` text NOT NULL,
+  `photo_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`attr_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `attractions`
+--
+
+LOCK TABLES `attractions` WRITE;
+/*!40000 ALTER TABLE `attractions` DISABLE KEYS */;
+INSERT INTO `attractions` VALUES (1,'Hurla-Whirl','2020-06-12','2023-12-06','Spins really super fast!',NULL);
+/*!40000 ALTER TABLE `attractions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+  `cart_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `item_id` int DEFAULT NULL,
+  `ticket_id` int DEFAULT NULL,
+  `quanitity` int NOT NULL DEFAULT '1',
+  `time_added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `is_active` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`cart_id`),
+  KEY `fk_user_id_cart` (`user_id`),
+  KEY `fk_item_id_cart` (`item_id`),
+  CONSTRAINT `fk_item_id_cart` FOREIGN KEY (`item_id`) REFERENCES `merchandise` (`item_id`),
+  CONSTRAINT `fk_user_id_cart` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `item_or_ticket` CHECK ((((`item_id` is not null) and (`ticket_id` is null)) or ((`item_id` is null) and (`ticket_id` is not null))))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `comments`
 --
 
@@ -44,6 +105,32 @@ LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
 INSERT INTO `comments` VALUES (1,1000,'I enjoyed it.',NULL,'2024-03-19 13:02:32'),(2,1001,'I did as well.',1,'2024-03-19 13:03:38');
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `merchandise`
+--
+
+DROP TABLE IF EXISTS `merchandise`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `merchandise` (
+  `item_id` int NOT NULL AUTO_INCREMENT,
+  `item_name` varchar(255) NOT NULL,
+  `number_in_stock` int NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `merchandise`
+--
+
+LOCK TABLES `merchandise` WRITE;
+/*!40000 ALTER TABLE `merchandise` DISABLE KEYS */;
+INSERT INTO `merchandise` VALUES (1,'Park T-Shirt S',100,20.00),(2,'Park T-Shirt M',100,20.00),(3,'Park T-Shirt L',100,20.00),(6,'Park T-Shirt XL',100,20.00),(7,'Park Branded Nalgene Water Bottle',100,25.00);
+/*!40000 ALTER TABLE `merchandise` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -141,4 +228,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-21 12:38:52
+-- Dump completed on 2024-03-25 22:38:00
