8a586364850e1808fa4f8732f69de0ae3e70d662
ARGamer36 <ajrodz2004@gmail.com>
2024-04-21 12:56:07 -0400

    Started Creating base functionality for cart

--- server/database.js
+++ server/database.js
@@ -15,13 +15,13 @@ async function getUsers() {
     const rows = result[0]
     // or: const [rows] = await pool.query("SELECT * FROM users")
     return rows
-} // getNotes
+} // getUsers
 
 // returns user info for given id
 async function getUser(id) {
     const [rows] = await pool.query("SELECT * FROM users WHERE id = ?", [id]) 
     return rows
-} // getNote
+} // getUser
 
 // returns user info for given username
 async function getUserByUsername(username) {
@@ -40,7 +40,7 @@ async function getId(username) {
     const [rows] = await pool.query("SELECT id FROM users WHERE username = ?", [username]) 
     return rows[0]
 } // getId
-
+4
 async function createUser(username, lastname, firstname, email, password) {
     const [result] = await pool.query("INSERT INTO users (username, last_name, first_name, email, password) VALUES (?, ?, ?, ?, ?)", [username, lastname, firstname, email, password])
     if (result.affectedRows > 0) {
@@ -63,11 +63,30 @@ async function registerUser(username, lastname, firstname, email, password) {
     return result
 } // registerUser   
 
+// Verifies that password matches too username
+async function verifyLogin(username, password) {
+    const [result] = await pool.query("SELECT password FROM users WHERE username = ?", [username]);
+    if (result && result.length > 0 && result[0].password === password) {
+        return true;
+    } else {
+        return false;
+    }
+} // verifyLogin
+
 async function getComments() {
     const result = await pool.query("SELECT * FROM comments")
     const rows = result[0]
     return rows
 } // getComments
+
+async function removeUser(username) {
+    const [result] = await pool.query("DELETE FROM users WHERE username = ?", [username])
+    if (result.changedRows > 0) {
+        return true; // user was successfully removed
+    } else {
+        return false; // No rows were updated, user deletion failed
+    }
+} // removeUser
 
 async function changeEmail(username, newEmail) {
     const user = await getId(username)
@@ -80,15 +99,6 @@ async function changeEmail(username, newEmail) {
       }
 } // changeEmail
 
-async function removeUser(username) {
-    const [result] = await pool.query("DELETE FROM users WHERE username = ?", [username])
-    if (result.changedRows > 0) {
-        return true; // user was successfully removed
-    } else {
-        return false; // No rows were updated, user deletion failed
-    }
-} // removeUser
-
 async function purchaseTicket(userId, ticketType, dateValid) {
     const [result] = await pool.query("INSERT INTO tickets (user_id, ticket_type, date_valid) VALUES (?,?,?)",[userId, ticketType, dateValid])
     if (result.changedRows > 0) {
@@ -98,14 +108,38 @@ async function purchaseTicket(userId, ticketType, dateValid) {
     }
 } // purchaseTicket
 
-async function verifyLogin(username, password) {
-    const [result] = await pool.query("SELECT password FROM users WHERE username = ?", [username]);
-    if (result && result.length > 0 && result[0].password === password) {
-        return true;
+// returns cart items of user given id
+async function getCart(username) {
+    const id = await getId(username);
+    const [rows] = await pool.query("SELECT * FROM cart WHERE user_id = ?", [id.id])
+    return rows
+} // getCartItems
+
+async function addCartItem(username, item_id, quantity) {
+    const user_id = await getId(username);
+    const time_added = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    const [result] = await pool.query("INSERT INTO cart (user_id, item_id, quanitity, time_added, is_active) VALUES (?, ?, ?, ?, ?)", [user_id.id, item_id, quantity, time_added, 1]);
+    if (result.affectedRows > 0) {
+        return true; // user was created
     } else {
-        return false;
+        return false; // user was not created
     }
-} // verifyLogin
+} // addCartItem
+
+async function removeCartItem(cart_id) {
+    const [result] = await pool.query("DELETE FROM cart WHERE cart_id = ?", [cart_id])
+    if (result.affectedRows > 0) {
+        return true; // user was successfully removed
+    } else {
+        return false; // No rows were updated, user deletion failed
+    }
+} // removeCartItem
+
+// Returns merchandise for given id
+async function getMerchandise(id) {
+    const [rows] = await pool.query("SELECT * FROM merchandise WHERE item_id = ?", [id]) 
+    return rows
+} // getMerchandise
 
 module.exports = {
     getUsers,
@@ -115,7 +149,11 @@ module.exports = {
     changeEmail, 
     purchaseTicket, 
     verifyLogin, 
-    getUserByUsername
+    getUserByUsername, 
+    getCart,
+    getMerchandise,
+    addCartItem,
+    removeCartItem
 };
 
 // testing
